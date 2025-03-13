fd747ed7fba1d7f92f2f077f632b4e0fa730c126
4$X <114998095+webRemi@users.noreply.github.com>
2025-03-13 21:44:48 +0100

    Update savelog.c

--- savelog/savelog.c
+++ savelog/savelog.c
@@ -5,6 +5,11 @@
 BOOL SaveLog(LPCSTR lpFileName, CHAR* cBuffer, SIZE_T sBufferSize) {
     HANDLE hFile = NULL;
     DWORD dwBytesWritten;
+    CHAR* cSeparatorStart = "=====START=====\n";
+    CHAR* cSeparatorEnd = "\n======END======\n";
+    SIZE_T sSeparatorStartSize = strlen(cSeparatorStart);
+    SIZE_T sSeparatorEndSize = strlen(cSeparatorEnd);
+
 
     printf("%s Writing to a file\n", INFO);
     hFile = CreateFileA(lpFileName, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
@@ -15,7 +20,15 @@ BOOL SaveLog(LPCSTR lpFileName, CHAR* cBuffer, SIZE_T sBufferSize) {
 
     SetFilePointer(hFile, 0, NULL, FILE_END);
 
+    if (!WriteFile(hFile, cSeparatorStart, sSeparatorStartSize, &dwBytesWritten, NULL)) {
+        printf("%s WriteFile failed with error: 0x%x\n", ERROR, GetLastError());
+        return 1;
+    }
     if (!WriteFile(hFile, cBuffer, sBufferSize, &dwBytesWritten, NULL)) {
+        printf("%s WriteFile failed with error: 0x%x\n", ERROR, GetLastError());
+        return 1;
+    }
+    if (!WriteFile(hFile, cSeparatorEnd, sSeparatorEndSize, &dwBytesWritten, NULL)) {
         printf("%s WriteFile failed with error: 0x%x\n", ERROR, GetLastError());
         return 1;
     }
